b26b7b636532b125c5276b25d736d1969556c0f0
caringrith <ingrithc@unicauca.edu.co>
2021-03-03 15:01:48 -0500

    Formulario Color, métodos para buscar color (general, codigo y descripción)

--- PedidoTela.Data/Acceso/D_Color.cs
+++ PedidoTela.Data/Acceso/D_Color.cs
@@ -1,5 +1,4 @@
-﻿using IBM.Data.Informix;
-using PedidoTela.Entidades.Logica;
+﻿using PedidoTela.Entidades.Logica;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +9,7 @@ namespace PedidoTela.Data.Acceso
 {
     public class D_Color
     {
-        #region Consultas
-        private readonly string consultarAll = "SELECT trim(codi_color) codi_color,upper(trim(desc_color)) desc_color " +
-            "FROM inmcolor;";
-        private readonly string consultarLikeCodigo = "SELECT trim(codi_color) codi_color,upper(trim(desc_color)) desc_color " +
-            "FROM inmcolor WHERE codi_color LIKE ?;";
-        private readonly string consultarLikeDescripcion = "SELECT trim(codi_color) codi_color,upper(trim(desc_color)) desc_color " +
-            "FROM inmcolor WHERE desc_color LIKE ?;";
-        private readonly string consultar = "select trim(codi_color) codi_color,upper(trim(desc_color)) desc_color,trim(codi_color) || ' - ' ||upper(trim(desc_color)) _codigonombre " +
+        private readonly string consultarAll = "select trim(codi_color) codi_color,upper(trim(desc_color)) desc_color,trim(codi_color) || ' - ' ||upper(trim(desc_color)) _codigonombre " +
             "from inmcolor e where(select count(*) from cfc_e_nocolor where idempresa = 1 and codi_color = e.codi_color) = 0 " +
             "and desc_color is not null and desc_color<> '' " +
             "and codi_color  matches '[0-9]' " +
@@ -41,7 +33,7 @@ namespace PedidoTela.Data.Acceso
             "from inmcolor e where(select count(*) from cfc_e_nocolor where idempresa = 1 and codi_color = e.codi_color) = 0  " +
             "and desc_color is not null and desc_color<> '' " +
             "and codi_color  matches '[0-9][0-9][0-9][0-9][0-9]'";
-        #endregion
+
 
         public void Actualizar(Objeto elemento)
         {
@@ -54,62 +46,6 @@ namespace PedidoTela.Data.Acceso
             using (var con = new clsConexion())
             {
                 var datosDataReader = con.EjecutarConsulta(consultarAll);
-                while (datosDataReader.Read())
-                {
-                    Objeto obj = new Objeto();
-                    obj.Id = datosDataReader["codi_color"].ToString();
-                    obj.Nombre = datosDataReader["desc_color"].ToString();
-                    respuesta.Add(obj);
-                };
-                con.cerrarConexion();
-            }
-            return respuesta;
-        }
-
-        public List<Objeto> buscarColorPorCodigo(string codigo)
-        {
-            List<Objeto> respuesta = new List<Objeto>();
-            using (var con = new clsConexion())
-            {
-                con.Parametros.Add(new IfxParameter("@codi_color", codigo + "%"));
-                var datosDataReader = con.EjecutarConsulta(consultarLikeCodigo);
-                while (datosDataReader.Read())
-                {
-                    Objeto obj = new Objeto();
-                    obj.Id = datosDataReader["codi_color"].ToString();
-                    obj.Nombre = datosDataReader["desc_color"].ToString();
-                    respuesta.Add(obj);
-                };
-                con.cerrarConexion();
-            }
-            return respuesta;
-        }
-
-        public List<Objeto> buscarColorPorDescripcion(string descripcion)
-        {
-            List<Objeto> respuesta = new List<Objeto>();
-            using (var con = new clsConexion())
-            {
-                con.Parametros.Add(new IfxParameter("@desc_color", descripcion + "%"));
-                var datosDataReader = con.EjecutarConsulta(consultarLikeDescripcion);
-                while (datosDataReader.Read())
-                {
-                    Objeto obj = new Objeto();
-                    obj.Id = datosDataReader["codi_color"].ToString();
-                    obj.Nombre = datosDataReader["desc_color"].ToString();
-                    respuesta.Add(obj);
-                };
-                con.cerrarConexion();
-            }
-            return respuesta;
-        }
-
-        public List<Objeto> obtenerColores()
-        {
-            List<Objeto> respuesta = new List<Objeto>();
-            using (var con = new clsConexion())
-            {
-                var datosDataReader = con.EjecutarConsulta(consultar);
                 while (datosDataReader.Read())
                 {
                     Objeto obj = new Objeto();
